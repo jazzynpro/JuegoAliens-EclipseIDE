1e2a8200ba775bb5ec8784b02c03620d66c44564
jazzynpro <reader_jaz@hotmail.com>
2026-08-15 17:27:45 -0500

    Constructores y calculo de precios

--- src/com/krakedev/Alien.java
+++ src/com/krakedev/Alien.java
@@ -7,5 +7,23 @@ public class Alien {
 	private int numeroBrazos;
 	private int numeroPies;
 	private double precioExtremidad;
+	private double precioOjo;
 	private double precioCuerpo;
+	
+//constructor que recibe tamaño y color
+	public Alien (int tamanio, String color) {
+		//validaciones
+		if(tamanio < 5) {
+			this.tamanio = 5;
+		}else if(tamanio > 30) {
+			this.tamanio = 30;
+		}else {
+			this.tamanio = tamanio;
+		}
+		// calculo de precios
+		this.color = color;
+		this.precioCuerpo = tamanio * 0.20;
+		this.precioExtremidad = tamanio * 0.10;
+		this.precioOjo = tamanio * 0.5;
+	}
 }
