32a9af85cb0b6bc5b68cdcaee3a71d19ec5126f5
jazzynpro <reader_jaz@hotmail.com>
2026-08-15 17:18:22 -0500

    Atributos Modelo del Alien

---
+++ src/com/krakedev/Alien.java
@@ -0,0 +1,11 @@
+package com.krakedev;
+//Atributos privados
+public class Alien {
+	private int tamanio;
+	private String color;
+	private int numeroOjos;
+	private int numeroBrazos;
+	private int numeroPies;
+	private double precioExtremidad;
+	private double precioCuerpo;
+}
